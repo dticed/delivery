e59c61724cdd76e411e189fb78997d06aedef728
dticed <lucaspierrealencar@gmail.com>
2021-03-02 19:08:05 -0300

    Adicionando docstring na factory

--- delivery/app.py
+++ delivery/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from delivery.ext import site
 
 def create_app():
+    """Factory to create a Flask app based on factory pattern"""
     app = Flask(__name__)
     site.init_app(app)
     return app
