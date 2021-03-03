09df5b512c7b8a6997116883132bc252c4e5c7d6
dticed <lucaspierrealencar@gmail.com>
2021-03-03 18:05:34 -0300

    Modificando status do debug para true

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ def test_app_is_created(app):
     
 # teste de config
 def test_config_is_loaded(config):
-    assert config['DEBUG'] is False
+    assert config['DEBUG'] is True
     
 # teste de client
 def test_request_returns_404(client):
